078fd6aa82e606e5c2c746642a0268ed8ec0c982
rrafids <raihanrfd1@gmail.com>
2019-11-25 19:03:22 +0700

    fix_comment

--- storage/framework/views/b8ca3f8774d5b9a226c3ce02d15f84878ed0b4c2.php
+++ storage/framework/views/b8ca3f8774d5b9a226c3ce02d15f84878ed0b4c2.php
@@ -180,4 +180,5 @@
   });
       </script>
   <?php $__env->stopSection(); ?>
+
 <?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /opt/lampp/htdocs/PlesirJogja/resources/views/Places/show.blade.php ENDPATH**/ ?>
